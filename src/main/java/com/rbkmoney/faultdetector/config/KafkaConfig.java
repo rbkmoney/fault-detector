5ad010959cfec2e3edfffc6727a4b56eac34526c
Baikov Dmitrii <44803026+D-Baykov@users.noreply.github.com>
2019-05-13 19:06:00 +0300

    Fix client.id property (#10)

--- src/main/java/com/rbkmoney/faultdetector/config/KafkaConfig.java
+++ src/main/java/com/rbkmoney/faultdetector/config/KafkaConfig.java
@@ -98,6 +98,7 @@ public class KafkaConfig {
     public ConsumerFactory<String, ServiceOperation> serviceOperationConsumerFactory() {
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, servers);
+        props.put(ConsumerConfig.CLIENT_ID_CONFIG, clientId);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ServiceOperationDeserializer.class);
