ce1f5619269f3bc70fd7a6ec12ee03ad05c56c73
Baikov Dmitrii <44803026+D-Baykov@users.noreply.github.com>
2019-07-04 20:24:57 +0300

    Fix kafka producer. Adding message key (#18)

--- src/main/java/com/rbkmoney/faultdetector/data/ServiceAggregates.java
+++ src/main/java/com/rbkmoney/faultdetector/data/ServiceAggregates.java
@@ -2,6 +2,7 @@ package com.rbkmoney.faultdetector.data;
 
 import lombok.Data;
 
+//TODO: сделать рефакторинг, как только подключиться хранилище
 @Data
 public class ServiceAggregates {
 
@@ -13,10 +14,22 @@ public class ServiceAggregates {
 
     private long operationsCount;
 
+    private long totalOperationsCount;
+
     private long successOperationsCount;
+
+    private long totalSuccessOperationsCount;
 
     private long errorOperationsCount;
 
+    private long totalErrorOperationsCount;
+
     private long overtimeOperationsCount;
+
+    private String overtimeOperationsProgressiveLine;
+
+    private String errorOperationsProgressiveLine;
+
+    private String operationsCountProgressiveLine;
 
 }
