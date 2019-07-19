ca3dbf5493bd0184d3a2cd3ec9bb31f8cd0e7dab
d.baykov <d.baykov@rbkmoney.com>
2019-07-19 17:57:27 +0300

    Refactor micrometrics gaude creation

---
+++ src/main/java/com/rbkmoney/faultdetector/data/FaultDetectorMetricsBinder.java
@@ -0,0 +1,89 @@
+package com.rbkmoney.faultdetector.data;
+
+import io.micrometer.core.instrument.Gauge;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.binder.MeterBinder;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import static java.util.Collections.emptyList;
+
+@Slf4j
+@RequiredArgsConstructor
+public class FaultDetectorMetricsBinder implements MeterBinder {
+
+    private final ServiceAggregates serviceAggregates;
+
+    private final String serviceId;
+
+    private static final String GAUGE_PREFEX = ".aggregate.";
+
+    private static final String FAILURE_GAUGE_NAME = GAUGE_PREFEX + "failure.rate.";
+
+    private static final String OPER_COUNT_GAUGE_NAME = GAUGE_PREFEX + "operations.count.";
+
+    private static final String SUCCESS_OPER_COUNT_GAUGE_NAME = GAUGE_PREFEX + "success.count.";
+
+    private static final String ERROR_OPER_COUNT_GAUGE_NAME = GAUGE_PREFEX + "error.count.";
+
+    private static final String OVERTIME_OPER_COUNT_GAUGE_NAME = GAUGE_PREFEX + "overtime.count.";
+
+    private static final String BASE_UNIT = "value";
+
+    private static final String GAUGE_LOG_PATTERN = "Gauge {} was added";
+
+    @Override
+    public void bindTo(MeterRegistry registry) {
+        registerFailureRateMetrics(registry);
+        registerOperCountMetrics(registry);
+        registerSuccessOperCountMetrics(registry);
+        registerErrorOperCountMetrics(registry);
+        registerOvertimeOperCountMetrics(registry);
+    }
+
+    private void registerFailureRateMetrics(MeterRegistry registry) {
+        Gauge.builder(serviceId + FAILURE_GAUGE_NAME, serviceAggregates, ServiceAggregates::getFailureRate)
+                .tags(emptyList())
+                .description("The value of the availability metric for the service " + serviceId)
+                .baseUnit(BASE_UNIT)
+                .register(registry);
+        log.info(GAUGE_LOG_PATTERN, serviceId + FAILURE_GAUGE_NAME);
+    }
+
+    private void registerOperCountMetrics(MeterRegistry registry) {
+        Gauge.builder(serviceId + OPER_COUNT_GAUGE_NAME, serviceAggregates, ServiceAggregates::getOperationsCount)
+                .tags(emptyList())
+                .description("The value of operations count for the service " + serviceId)
+                .baseUnit(BASE_UNIT)
+                .register(registry);
+        log.info(GAUGE_LOG_PATTERN, serviceId + OPER_COUNT_GAUGE_NAME);
+    }
+
+    private void registerSuccessOperCountMetrics(MeterRegistry registry) {
+        Gauge.builder(serviceId + SUCCESS_OPER_COUNT_GAUGE_NAME, serviceAggregates, ServiceAggregates::getSuccessOperationsCount)
+                .tags(emptyList())
+                .description("The value of success operations count for the service " + serviceId)
+                .baseUnit(BASE_UNIT)
+                .register(registry);
+        log.info(GAUGE_LOG_PATTERN, serviceId + SUCCESS_OPER_COUNT_GAUGE_NAME);
+    }
+
+    private void registerErrorOperCountMetrics(MeterRegistry registry) {
+        Gauge.builder(serviceId + ERROR_OPER_COUNT_GAUGE_NAME, serviceAggregates, ServiceAggregates::getErrorOperationsCount)
+                .tags(emptyList())
+                .description("The value of error operations count for the service " + serviceId)
+                .baseUnit(BASE_UNIT)
+                .register(registry);
+        log.info(GAUGE_LOG_PATTERN, serviceId + ERROR_OPER_COUNT_GAUGE_NAME);
+    }
+
+    private void registerOvertimeOperCountMetrics(MeterRegistry registry) {
+        Gauge.builder(serviceId + OVERTIME_OPER_COUNT_GAUGE_NAME, serviceAggregates, (aggregates) -> aggregates.getErrorOperationsCount())
+                .tags(emptyList())
+                .description("The value of overtime operations count for the service " + serviceId)
+                .baseUnit(BASE_UNIT)
+                .register(registry);
+        log.info(GAUGE_LOG_PATTERN, serviceId + OVERTIME_OPER_COUNT_GAUGE_NAME);
+    }
+
+}
