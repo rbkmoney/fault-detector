5127746f362ca486c263dc1bc673e37eed9d4f3f
d.baykov <d.baykov@rbkmoney.com>
2019-12-04 17:31:59 +0300

    BJ-676: Revert metric registry to basic

--- src/main/java/com/rbkmoney/faultdetector/config/AppConfig.java
+++ src/main/java/com/rbkmoney/faultdetector/config/AppConfig.java
@@ -4,22 +4,14 @@ import com.rbkmoney.faultdetector.data.ServiceAggregates;
 import com.rbkmoney.faultdetector.data.ServiceOperations;
 import com.rbkmoney.faultdetector.data.ServicePreAggregates;
 import com.rbkmoney.faultdetector.data.ServiceSettings;
-import io.micrometer.core.instrument.Clock;
-import io.micrometer.statsd.StatsdConfig;
-import io.micrometer.statsd.StatsdMeterRegistry;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.actuate.autoconfigure.metrics.export.statsd.StatsdProperties;
-import org.springframework.boot.actuate.autoconfigure.metrics.export.statsd.StatsdPropertiesConfigAdapter;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-
-import static io.micrometer.shaded.io.netty.util.internal.StringUtil.EMPTY_STRING;
 
 @Slf4j
 @Configuration
@@ -61,28 +53,6 @@ public class AppConfig {
         ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
         taskScheduler.setPoolSize(schedulerPoolSize);
         return taskScheduler;
-    }
-
-    @Bean
-    @ConditionalOnMissingBean
-    public StatsdConfig fdStatsdConfig(StatsdProperties statsdProperties) {
-        String host = statsdProperties == null ? EMPTY_STRING : statsdProperties.getHost();
-        Integer port = statsdProperties == null ? 0 : statsdProperties.getPort();
-        log.info("Create new StatsdConfig by StatsdProperties for host {} and port {}", host, port);
-        return new StatsdPropertiesConfigAdapter(statsdProperties);
-    }
-
-    @Bean
-    @ConditionalOnMissingBean
-    public StatsdMeterRegistry fdStatsdMeterRegistry(StatsdConfig fdStatsdConfig,
-                                                     Clock clock) {
-        try {
-            log.info("Create new StatsdMeterRegistry by StatsdConfig: {}", fdStatsdConfig);
-            return new StatsdMeterRegistry(fdStatsdConfig, clock);
-        } catch (Exception ex) {
-            log.error("Received error in StatsdMeterRegistry", ex);
-            throw ex;
-        }
     }
 
 }
