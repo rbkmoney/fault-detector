07834b9b4c29a5940d90d069b17edcdf9b80f4a9
d.baykov <d.baykov@rbkmoney.com>
2020-01-24 13:50:18 +0300

    BJ-770: Fix service build

--- src/main/java/com/rbkmoney/faultdetector/services/FaultDetectorMetricsService.java
+++ src/main/java/com/rbkmoney/faultdetector/services/FaultDetectorMetricsService.java
@@ -3,6 +3,8 @@ package com.rbkmoney.faultdetector.services;
 import com.rbkmoney.faultdetector.data.ServiceAggregates;
 import com.rbkmoney.faultdetector.handlers.Handler;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -10,8 +12,10 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
+@Lazy
 @Service
 @RequiredArgsConstructor
+@ConditionalOnProperty(value = "management.metrics.export.statsd.enabled", havingValue = "true")
 public class FaultDetectorMetricsService {
 
     private final Map<String, ServiceAggregates> aggregatesMap;
